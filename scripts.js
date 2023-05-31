bfa12efe00bf05d4239b716bf7f9adb28fecc39d
devhnry <devwhenry@gmail.com>
2023-05-31 21:10:45 +0100

    Completed Desktop and Mobile Navbar

--- scripts.js
+++ scripts.js
@@ -1,21 +1,51 @@
-import '/output.css'
+import "/output.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
+const mobileNav = document.querySelector(".mobile-nav");
+const overlay = document.querySelector(".overlay");
 
-setupCounter(document.querySelector('#counter'))
+const openNav = function () {
+  menuOpen.classList.add("hidden");
+  menuClose.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  setTimeout(() => {
+    overlay.classList.remove("hidden");
+  }, 500);
+  mobileNav.classList.remove("hidden");
+  mobileNav.classList.add("scaleUp");
+  mobileNav.classList.remove("scaleDown");
+  overlay.classList.add("fadein");
+  overlay.classList.remove("fadeout");
+  document.querySelector("body").style.overflowY = "hidden";
+};
+
+const closeNav = function () {
+  menuOpen.classList.remove("hidden");
+  menuClose.classList.add("hidden");
+  mobileNav.classList.remove("scaleUp");
+  mobileNav.classList.add("scaleDown");
+  overlay.classList.remove("fadein");
+  overlay.classList.add("fadeout");
+  setTimeout(() => {
+    mobileNav.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }, 500);
+  document.querySelector("body").style.overflowY = "visible";
+};
+
+menuOpen.addEventListener("click", openNav);
+menuClose.addEventListener("click", closeNav);
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768){
+      menuOpen.classList.remove("hidden");
+      menuClose.classList.add("hidden");
+      mobileNav.classList.add("scaleUp");
+      mobileNav.classList.remove("scaleDown");
+      mobileNav.classList.add("hidden");
+      overlay.classList.add("hidden");
+      overlay.classList.add("fadein")
+      overlay.classList.remove("fadeout");
+  }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ const plugin = require("tailwindcss/plugin");
 export default {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens : {
+      sm : "600px",
+      md: "768px",
+    },
     colors: {
       "dark-blue": "hsl(233, 26%, 24%)",
       green: "hsl(136, 65%, 51%)",
@@ -13,7 +17,7 @@ export default {
       "cyan-light": "hsl(192, 70%, 75%)",
 
       "neutral-400": "hsl(233, 8%, 62%)",
-      "neutral-200": "hsl(220, 16%, 96%)",
+      "neutral-300": "hsl(220, 16%, 96%)",
       "neutral-200": "hsl(0, 0%, 98%)",
       white: " hsl(0, 0%, 100%)",
     },
@@ -21,8 +25,9 @@ export default {
     fontFamily : {
       main : ['Public Sans', 'sans-serif']
     },
-    
-    extend: {},
+
+    extend: {
+    },
   },
   plugins: [
     plugin(function ({ addBase }) {
