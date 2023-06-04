8c6f692f73e3c649f6f0bfa70ffd43b4c3991217
devhnry <devwhenry@gmail.com>
2023-06-04 22:29:10 +0100

    Refactored JS File, Responsiveness

--- scripts.js
+++ scripts.js
@@ -1,23 +1,19 @@
-import "/output.css";
-
 const menuOpen = document.querySelector(".menu-open");
 const menuClose = document.querySelector(".menu-close");
 const mobileNav = document.querySelector(".mobile-nav");
 const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
 
 const openNav = () => {
   menuOpen.classList.add("hidden");
   menuClose.classList.remove("hidden");
   overlay.classList.remove("hidden");
-  setTimeout(() => {
-    overlay.classList.remove("hidden");
-  }, 400);
+  overlay.classList.remove("fadeout");
+  overlay.classList.add("fadein");
   mobileNav.classList.remove("hidden");
   mobileNav.classList.add("scaleUp");
   mobileNav.classList.remove("scaleDown");
-  overlay.classList.add("fadein");
-  overlay.classList.remove("fadeout");
-  document.querySelector("body").style.overflowY = "hidden";
+  body.style.overflowY = "hidden";
 };
 
 const closeNav = () => {
@@ -31,21 +27,26 @@ const closeNav = () => {
     mobileNav.classList.add("hidden");
     overlay.classList.add("hidden");
   }, 400);
-  document.querySelector("body").style.overflowY = "visible";
+  body.style.overflowY = "visible";
+};
+
+const resetNav = () => {
+  menuOpen.classList.remove("hidden");
+  menuClose.classList.add("hidden");
+  mobileNav.classList.add("hidden");
+  mobileNav.classList.remove("scaleUp");
+  mobileNav.classList.remove("scaleDown");
+  overlay.classList.add("hidden");
+  overlay.classList.remove("fadein");
+  overlay.classList.remove("fadeout");
+};
+
+const handleWindowResize = () => {
+  if (window.innerWidth > 768) {
+    resetNav();
+  }
 };
 
 menuOpen.addEventListener("click", openNav);
 menuClose.addEventListener("click", closeNav);
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 768){
-      menuOpen.classList.remove("hidden");
-      menuClose.classList.add("hidden");
-      mobileNav.classList.add("scaleUp");
-      mobileNav.classList.remove("scaleDown");
-      mobileNav.classList.add("hidden");
-      overlay.classList.add("hidden");
-      overlay.classList.add("fadein")
-      overlay.classList.remove("fadeout");
-  }
-})
+window.addEventListener("resize", handleWindowResize);
